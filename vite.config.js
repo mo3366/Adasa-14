dafb91b549e3517b3a275d15c2b6f6f84f21a5a4
Momen <momena881@gmail.com>
2026-01-28 23:17:58 +0200

    Add build and update vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Adasa-14/',  // << مهم جداً، لازم يكون نفس اسم الريبو
 })
