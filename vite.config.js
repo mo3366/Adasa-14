c9ae7090088a184d714a85d748371f6836caae98
Momen <momena881@gmail.com>
2026-01-28 23:30:46 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Adasa-14/',  // << مهم جداً، لازم يكون نفس اسم الريبو
+  base: '/MyReactApp/', // <-- نفس اسم الريبو الجديد
 })
